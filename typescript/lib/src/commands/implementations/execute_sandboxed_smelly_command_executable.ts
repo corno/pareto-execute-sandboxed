eccc67b1de2f5e773c81eb0d15892bd3533c8619
Corno Schraverus <corno@schraverus.com>
2026-08-07 15:42:03 +0200

    first commit

---
+++ typescript/lib/src/commands/implementations/execute_sandboxed_smelly_command_executable.ts
@@ -0,0 +1,26 @@
+import * as p_ from 'pareto-core/implementation/command'
+
+import type * as command_interfaces from "../interfaces.js"
+import type * as command_interfaces_resources from "pareto-resources/commands/interfaces"
+
+export const $$: p_.Command_Implementation<
+    command_interfaces.smelly_command_executable,
+    {
+        'program': string,
+    },
+    null,
+    {
+        'unrestricted': command_interfaces_resources.execute_unrestricted.smelly_command_executable,
+    }
+> = p_.command(
+    ($d, $s, $q, $c) => [
+        $c.unrestricted.execute(
+            {
+                'program': $s.program,
+                'args': $d.args,
+                'working directory': $d['working directory']
+            },
+            ($) => $
+        )
+    ]
+)
---
+++ typescript/lib/src/queries/implementations/execute_sandboxed_query_executable.ts
@@ -0,0 +1,26 @@
+import * as p_ from 'pareto-core/implementation/query'
+
+import type * as query_interfaces from "../interfaces.js"
+import type * as query_interfaces_resources from "pareto-resources/queries/interfaces"
+
+//dependencies
+
+
+export const $$: p_.Query_Implementation<
+    query_interfaces.query_executable,
+    {
+        'program': string,
+    },
+    {
+        'unrestricted': query_interfaces_resources.execute_unrestricted.query_executable,
+    }
+> = p_.query(
+    ($d, $s, $q) => $q.unrestricted(
+        {
+            'program': $s.program,
+            'args': $d.args,
+            'working directory': $d['working directory']
+        },
+        ($) => $,
+    )
+)
